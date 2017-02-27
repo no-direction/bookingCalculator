874b786601d835517718bf10e199f12369469c8d
Tim Clifford <dutchbantam17@gmail.com>
2017-02-27 20:13:01 +0000

    Adjusting the calculation

--- src/com/example/booking/BookingCalc.java
+++ src/com/example/booking/BookingCalc.java
@@ -6,7 +6,7 @@ public class BookingCalc {
 
     public static double calculate(int var0, int var1, int var2, int var3, int var4) {
         if(var2 <= 1000000) {
-            int baseCost = (var0 * var1);
+            int baseCost = (var0 * var1) / 2;
             int timesDays = (baseCost * var4);
             int extras = timesDays + var2 + var3;
             return extras;
